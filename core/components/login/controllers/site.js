1eb5d684c9a9c5be12e1f7846cf4da415621efba
paulo-sales <paulo.vitor@cetax.com.br>
2015-12-17 18:07:18 -0200

    Atualização 3 17/12/2015

--- core/components/login/controllers/site.js
+++ core/components/login/controllers/site.js
@@ -24,7 +24,7 @@ angular.module('mm.core.login')
 .controller('mmLoginSiteCtrl', function($scope, $state, $mmSitesManager, $mmUtil, $translate, $ionicHistory, $mmApp,
         $ionicModal, $mmLoginHelper) {
 
-    $scope.siteurl = '';
+    $scope.siteurl = 'http://104.155.40.71/';
     $scope.isInvalidUrl = true;
 
     $scope.validate = function(url) {
